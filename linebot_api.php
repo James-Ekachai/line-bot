ab75b62c6318a6639dae3b1fcd98027e75f84a9f
James-Ekachai <73385266+James-Ekachai@users.noreply.github.com>
2020-10-24 17:45:02 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -32,7 +32,7 @@ foreach ($request_json['events'] as $event)
 		   $obj = json_decode($result);
 		   
 		   $reply_message = $result;
-		   $reply_message = 'ตาย'. $obj->{'Deaths'};
+		   $reply_message = 'ตาย'. $obj->{'Deaths'}.'คน.';
 	   }
 			if($text == "@bot สวัสดี !!"){
 					$reply_message = "สวัสดีเจมส์";
